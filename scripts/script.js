a7a2534f2e60c7dae985fccfcfef2fcba82d2867
shayaankn <shayaankhan11@gmail.com>
2024-06-24 16:30:09 +0530

    Initial commit

--- scripts/script.js
+++ scripts/script.js
@@ -32,7 +32,7 @@ const container = document.querySelector(".counters");
 let activated = false;
 
 window.addEventListener("scroll", () => {
-    if (pageYOffset > container.offsetTop - container.offsetHeight - 500 && activated === false) {
+    if (pageYOffset > container.offsetTop - container.offsetHeight - 700 && activated === false) {
         counters.forEach(counter => {
             counter.innerText = 0;
             let count = 0;
@@ -48,15 +48,53 @@ window.addEventListener("scroll", () => {
                     counter.innerText = target;
                 }
             }
-        updateCount()
-        activated = true;
+            updateCount()
+            activated = true;
         });
 
     }
-    else if (pageYOffset < container.offsetTop - container.offsetHeight - 700 || pageYOffset === 0 && activated === true) {
+    else if (pageYOffset < container.offsetTop - container.offsetHeight - 1000 || pageYOffset === 0 && activated === true) {
         counters.forEach(counter => {
             counter.innerText = 0;
         });
         activated = false;
     }
 })
+
+// BACK TO TOP
+let backToTopBtn = document.getElementById("backToTopBtn");
+
+// When the user scrolls down 20px from the top of the document, show the button
+window.onscroll = function() {scrollFunction()};
+
+function scrollFunction() {
+    if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
+        backToTopBtn.style.display = "block";
+    } else {
+        backToTopBtn.style.display = "none";
+    }
+}
+
+// When the user clicks on the button, scroll to the top of the document
+backToTopBtn.onclick = function() {
+    document.body.scrollTop = 0; // For Safari
+    document.documentElement.scrollTop = 0; // For Chrome, Firefox, IE, and Opera
+}
+
+// DROPDOWN 
+
+// document.addEventListener("DOMContentLoaded", function() {
+//     const dropdownToggle = document.querySelector('.dropdown-toggle-special');
+//     const dropdownMenu = document.querySelector('.dropdown-menu-special');
+
+//     dropdownToggle.addEventListener('click', function() {
+//         dropdownMenu.classList.toggle('show');
+//     });
+
+//     // Close the dropdown menu if clicked outside
+//     document.addEventListener('click', function(event) {
+//         if (!dropdownToggle.contains(event.target) && !dropdownMenu.contains(event.target)) {
+//             dropdownMenu.classList.remove('show');
+//         }
+//     });
+// });
