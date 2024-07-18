5c184142ba240e164fe0de369417eee45c5f1fcd
shayaankn <159654471+shayaankn@users.noreply.github.com>
2024-07-18 17:29:46 +0530

    Custom carousel added
    Custom carousel added (home page) - still in development

--- assets/scripts/script.js
+++ assets/scripts/script.js
@@ -220,3 +220,26 @@ document.addEventListener('DOMContentLoaded', function() {
         });
     });
 });
+
+//======================================
+//-------------NEW CAROUSEL-------------
+//======================================
+
+
+document.addEventListener('DOMContentLoaded', function () {
+    var carousel = document.getElementById('carouselExampleIndicators');
+    var carouselListItems = document.querySelectorAll('#carousel-list-div .carousel-list-item-container');
+
+    carousel.addEventListener('slide.bs.carousel', function (event) {
+        // Get the index of the active slide
+        var slideIndex = event.to;
+
+        // Remove active class from all list items
+        carouselListItems.forEach(function (item) {
+            item.classList.remove('active');
+        });
+
+        // Add active class to the corresponding list item
+        carouselListItems[slideIndex].classList.add('active');
+    });
+});
